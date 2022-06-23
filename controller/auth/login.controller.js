3bc5676d021bb8c9d94f631accf0255d2a9018f7
mrnpro <yegetanaty03@gmail.com>
2022-06-24 00:35:33 +0300

    registeration updated

--- controller/auth/login.controller.js
+++ controller/auth/login.controller.js
@@ -2,24 +2,43 @@ const { getUser } = require('../../model/users.model')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const login = async(req, res) => {
-    const { email, password } = req.body;
-    const user = await getUser(email);
-    if (!user) {
-        res.status(400).send("Invalid email or password");
-    } else if (await bcrypt.compare(password, user.password)) {
-        const token = jwt.sign({ id: user._id, email: user.email },
-            process.env.JWT_SECRET, { expiresIn: "30d" }
-        );
+    const authheader = req.headers.auth;
 
-        res.status(200).send({
-            token,
-            "msg": "login success"
-        })
+    if (authheader) {
+        jwt.verify(authheader, process.env.JWT_SECRET, (error, user) => {
+            if (error) {
+                return res.status(400).send({ "msg": "authorization failed" })
+            }
+            email = user.email;
+            return res.status(200).send({ "msg": "login success" });
 
+        });
     } else {
-        res.status(400).send("")
-    }
 
+        var { email, password } = req.body;
+        const user = await getUser(email);
+        if (!user) {
+            res.status(400).send("Invalid email or password");
+        } else if (await bcrypt.compare(password, user.password)) {
+            try {
+                const token = jwt.sign({ id: user._id, email: user.email },
+                    process.env.JWT_SECRET, { expiresIn: "30d" }
+                );
+                res.setHeader('Content-Type', 'application/json');
+                return res.status(200).send({
+                    token,
+                    "msg": "login success"
+                });
+            } catch (e) {
+                return res.status(200).send({ msg: e });
+            }
+
+        } else {
+            return res.status(400).send("Invalid email or password ")
+        }
+
+
+    }
 
 
 }
--- controller/auth/register.controller.js
+++ controller/auth/register.controller.js
@@ -1,20 +1,29 @@
-const { adduser } = require('../../model/users.model')
+const { adduser, getUser } = require('../../model/users.model')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const register = async(req, res) => {
     const { fullname, email, password } = req.body;
-    if (fullname.length < 5) {
-        res.status(400).send({
-            "msg": "please enter proper name "
-        });
-    }
-    if (password.length < 8) {
-        res.status(400).send({
-            "msg": "please enter secure password"
-        });
-    }
+
     const encryptedPassword = await bcrypt.hash(password, 10);
-    const result = await adduser(fullname, email, encryptedPassword)
-    res.send(result)
+    const result = await adduser(fullname, email, encryptedPassword);
+
+    try {
+        if (result != "user already exists") {
+            const user = getUser(email)
+            if (user) {
+                const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+                return res.status(200).send({
+                    token,
+                    "msg": "success"
+                })
+            }
+        }
+    } catch (e) {
+        return res.status(400).send({ msg: e });
+    }
+
+    return res.status(400).send({ msg: result });
 }
 
 module.exports = register;

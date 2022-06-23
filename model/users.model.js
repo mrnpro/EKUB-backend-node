9fc42721fcf8463a91eb0b900f1e80e8ecb8e6bc
mrnpro <yegetanaty03@gmail.com>
2022-06-24 01:57:33 +0300

    update

--- model/users.model.js
+++ model/users.model.js
@@ -9,20 +9,33 @@ const adduser = async(fullname, email, password) => {
     try {
         await client.connect()
 
-        console.log("connected")
+
         const userAlreadyExists = await db.collection(collectionName).findOne({ email });
         if (userAlreadyExists) {
             console.log("user already axists");
             return "user already exists";
-
         }
-        const result = await db.collection(collectionName).insertOne({
+        const user = await db.collection(collectionName).insertOne({
             fullname,
             email,
             password
         });
+
+
+        //get user
+        const fetchedUser = await getUser(email);
+        if (fetchedUser) {
+            await client.connect()
+            const userId = fetchedUser._id;
+            await db.collection("Accounts").insertOne({
+                userId,
+                balance: "0",
+                package: "none"
+
+            })
+        }
         client.close();
-        return result;
+        return user;
     } catch (e) {
         console.log(e);
     }

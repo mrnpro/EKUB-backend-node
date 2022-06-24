4389488d2f0ede6011b58a42dff8394239e04271
mrnpro <yegetanaty03@gmail.com>
2022-06-24 22:52:41 +0300

    other routes are aded

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const auth_router = require("./routes/auth.router")
+const user_router = require('./routes/user.router');
+const account_router = require('./routes/account.router');
+const choosePackage = require('./routes/choosePackage.router');
+const history = require('./routes/history.router');
+const recharge = require('./routes/recharge.router');
+
 
 const app = express();
-app.use(express.json())
-app.use("/auth", auth_router)
+app.use(express.json());
 
-
+app.use("/auth", auth_router);
+app.use("/user", user_router);
+app.use('/account', account_router);
+app.use('/choosePackage', choosePackage);
+app.use('/history', history);
+app.use('/recharge', recharge);
 
 
 module.exports = { app, };
---
+++ routes/account.router.js
@@ -0,0 +1,5 @@
+ const express = require('express');
+ const route = express.Router();
+ route.get('/info');
+
+ module.exports = route;

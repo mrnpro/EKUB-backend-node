b9eac24b6d62fe2dd3fde0fd50d4164b696696a4
mrnpro <yegetanaty03@gmail.com>
2022-07-04 23:16:50 +0300

    update

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ require('dotenv').config()
 const { app } = require("./app");
 const port = 6000;
 
-app.listen(process.env.PORT || port, '0.0.0.0', () => { console.log(`Server started on port ${port}`) })
+app.listen(process.env.PORT || port, () => { console.log(`Server started on port ${port}`) })
